cdd53be397f1f7b2b0ca95a23061b70028cb6a98
Earthson Lu <Earthson.Lu@gmail.com>
2014-04-26 17:42:17 +0800

    add LDAMapper

--- src/main/scala/net/earthson/nlp/lda/adlda.scala
+++ src/main/scala/net/earthson/nlp/lda/adlda.scala
@@ -1,34 +1,53 @@
 package net.earthson.nlp.lda
 
 import scala.collection.mutable
-import net.earthson.nlp.util.MultiSampler
+import net.earthson.nlp.sampler.MultiSampler
+
+/*
+    val nzw = mutable.Map(mwz.groupBy(x=>(x._3, x._2)).mapValues(_.size).toSeq:_*).withDefaultValue(0)
+    val nz = mutable.Map(nzw.toSeq.map(x=>(x._1._1, x._2)).groupBy(_._1).mapValues(_.map(_._2).sum).toSeq:_*).withDefaultValue(0)
+    val nmz = mutable.Map(mwz.groupBy(x=>(x._1, x._3)).mapValues(_.size).toSeq:_*).withDefaultValue(0)
+    val nm = mutable.Map(nmz.toSeq.map(x=>(x._1._1, x._2)).groupBy(_._1).mapValues(_.map(_._2).sum).toSeq:_*).withDefaultValue(0)
+*/
 
 
-class ADLDAModel(k:Int, mwz:Seq[(Int,String,Int)]) {
-    var m_w_z = mwz
-    val n_z_w = mutable.Map(m_w_z.groupBy(x=>(x._3, x._2)).mapValues(_.size).toSeq:_*).withDefaultValue(0)
-    val n_z = mutable.Map(n_z_w.toSeq.map(x=>(x._1._1, x._2)).groupBy(_._1).mapValues(_.map(_._2).sum).toSeq:_*).withDefaultValue(0)
-    val n_m_z = mutable.Map(m_w_z.groupBy(x=>(x._1, x._3)).mapValues(_.size).toSeq:_*).withDefaultValue(0)
-    val n_m = mutable.Map(n_m_z.toSeq.map(x=>(x._1._1, x._2)).groupBy(_._1).mapValues(_.map(_._2).sum).toSeq:_*).withDefaultValue(0)
-    val alpha = n_m.size/50.0
+class ModelInfo(ntpcs:Int, ntms:Int) {
+    val ntopics = ntpcs
+    val nterms = ntms
+    val alpha = ntpcs/50.0
     val beta = 0.01
-    val topic_k = k
-    val n_term = m_w_z.map(x=>x._2).toSet.size
+}
 
-    def gibbsSampling() {
-        val new_nwz = for((g_m, g_w, g_z) <- m_w_z) yield {
-            n_z_w((g_z,g_w)) -= 1
-            n_z(g_z) -= 1
-            n_m_z((g_m,g_z)) -= 1
-            n_m(g_m) -= 1
-            val prob_z = for(z <- 1 to topic_k) yield ((n_z_w((z,g_w))+beta)/(n_z(z)+n_term*beta))*(n_m_z((g_m,z))+alpha)
-            val new_z = MultiSampler.multisampling(prob_z.toList)
-            n_z_w((new_z,g_w)) += 1
-            n_z(new_z) += 1
-            n_m_z((g_m, new_z)) += 1
-            n_m(g_m) += 1
-            (g_m, g_w, new_z)
+class TopicInfo(nzwo:Seq[((Int,String),Long)], nzo:Seq[(Int, Long)]) {
+    val nzw = nzwo
+    val nz = nzo
+}
+
+object LDAMapper {
+    def mapper(modelInfo: ModelInfo, topicinfo:TopicInfo, mwzo:Seq[(Long,String,Int)], round:Int) {
+        import modelInfo._
+        val mwz = mwzo.toBuffer
+        val nzw = mutable.Map(topicinfo.nzw:_*).withDefaultValue(0)
+        val nz = mutable.Map(topicinfo.nz:_*).withDefaultValue(0)
+        val nmz = mutable.Map(mwz.groupBy(x=>(x._1, x._3)).mapValues(_.size).toSeq:_*).withDefaultValue(0)
+        val nm = mutable.Map(nmz.toSeq.map(x=>(x._1._1, x._2)).groupBy(_._1).mapValues(_.map(_._2).sum).toSeq:_*).withDefaultValue(0)
+        for(r <- 1 to round) {
+            for(i <- 0 until mwz.size) {
+                val (cm, cw, cz) = mwz(i)
+                nzw((cz, cw)) -= 1
+                nz(cz) -= 1
+                nmz((cm,cz)) -= 1
+                //nm(cm) -= 1
+                val probz = for(z <- 0 until ntopics) yield ((nzw((z,cw))+beta)/(nz(z)+nterms*beta))*(nmz((cm,z))+alpha)
+                val newz = MultiSampler.multisampling(probz.toList)
+                mwz(i) = (cm, cw, newz)
+                nzw((newz,cw)) += 1
+                nz(newz) += 1
+                nmz((cm,newz)) += 1
+                //nm(cm) += 1
+            }
         }
-        this.m_w_z = new_nwz
+        mwz.toSeq
     }
+
 }
